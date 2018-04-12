08fdee7b25fb302283e4984629dae9e1b8d48bb1
sr320 <roberts.sbr@gmail.com>
2018-04-12 10:23:06 -0700

    removed sql from dplyr

--- R-projects/week3/scripts/me_in_class.R
+++ R-projects/week3/scripts/me_in_class.R
@@ -58,6 +58,28 @@ shrub_data$length*width*height
 
 # Excercises
 
+
+#-- Bird Banding --
+
+
+number_of_birds <- c(28, 32, 1, 0, 10, 22, 30, 19, 145, 27, 
+                     36, 25, 9, 38, 21, 12, 122, 87, 36, 3, 0, 5, 55, 62, 98, 32, 
+                     900, 33, 14, 39, 56, 81, 29, 38, 1, 0, 143, 37, 98, 77, 92, 
+                     83, 34, 98, 40, 45, 51, 17, 22, 37, 48, 38, 91, 73, 54, 46,
+                     102, 273, 600, 10, 11)
+
+length(number_of_birds)
+
+number_of_birds[42]
+
+
+number_of_birds[length(number_of_birds)]
+
+sum(number_of_birds)
+
+min(number_of_birds)
+
+
 # Shrub Volume Vector
 
 length <- c(2.2, 2.1, 2.7, 3.0, 3.1, 2.5, 1.9, 1.1, 3.5, 2.9)
@@ -71,10 +93,10 @@ sum(length*width*height)
 
  #A vector of the height of shrubs with lengths greater than 2.5.
 
-height > 2.5 # will return logicals with TRUE for the indices that meet the condition
+length > 2.5 # will return logicals with TRUE for the indices that meet the condition
 
 ## so we can use this to select only the values above 50
-height[height > 2.5]
+height[length > 2.5]
 
 # Ex 9 - Shrub Volume Data Frame --
 #link to question - https://sr320.github.io/course-fish497-2018/exercises/Data-frames-shrub-volume-data-frame-R/
