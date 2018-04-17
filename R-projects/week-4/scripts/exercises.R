03d5ec18bd3f844f5db61f0c2ae9dd30c62e285c
sr320 <roberts.sbr@gmail.com>
2018-04-17 11:22:12 -0700

    running through exercises

--- R-projects/week-4/scripts/exercises.R
+++ R-projects/week-4/scripts/exercises.R
@@ -57,5 +57,60 @@ mutate(shrub_dim, volumes = length * width * height)
 
 
 
-#
+#-- Shrub Volume Aggregation --
+#This is a follow-up to Shrub Volume Data Basics.
+
+#Dr. Granger wants some summary data of the plants at her sites and for her experiments. Make sure you have her shrub dimensions data.
+
+#This code calculates the average height of a plant at each site:
+  
+  by_site <- group_by(shrub_dim, site)
+  avg_height <- summarize(by_site, avg_height = mean(height))
+
+#Modify the code to calculate and print the average height of a plant in each experiment.
+
+  by_experiment <- group_by(shrub_dim, experiment)
+  avg_height <- summarize(by_experiment, avg_height = mean(height))
+  
+  avg_height
+#Use max() to determine the maximum height of a plant at each site.
+
+  max_height <- summarize(by_site, max_height = max(height))
+
+  max_height  
+  
+
+#  -- Shrub Volume Join --
+  #  This is a follow-up to Shrub Volume Aggregation.
+  
+ # Dr. Granger has kept a separate table that describes the manipulation for each experiment. Add the experiments data to your data folder.
+  
+  #Import the experiments data and then use inner_join to combine it with the shrub dimensions data to add a manipulation column to the shrub data.
+  
+svtable <-  read.csv("data/shrub-volume-experiments-table.csv")
+
+svtable
+shrub_dim
+
+inner_join(shrub_dim, svtable, by = "experiment")
+
+
+#-- Fix the Code --
+#  This is a follow-up to Shrub Volume Aggregation. If you haven’t already downloaded the shrub volume data do so now and store it in your data directory.
+
+#The following code is supposed to import the shrub volume data and calculate the average shrub volume for each site and, separately, for each experiment
+
+read.csv("data/shrub-volume-experiment.csv")
+shrub_data %>%
+  mutate(volume = length * width * height) %>%
+  group_by(site) %>%
+  summarize(mean_volume = max(volume))
+shrub_data %>%
+  mutate(volume = length * width * height)
+group_by(experiment) %>%
+  summarize(mean_volume = mean(volume))
+Fix the errors in the code so that it does what it’s supposed to
+Add a comment to the top of the code explaining what it does
+In a text file, discuss how you know that your fixed version of the code is right and how you would try to make sure it was right if the data file was thousands of lines long
+
 
